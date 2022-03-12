869007d7b8bdd8f249e897004fe9f695ee0f326c
Yoo Choi <dchoiboi00@gmail.com>
2022-03-12 15:56:03 +0900

    added aboutinfo data

---
+++ src/data/aboutInfo.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 0,
+        name: "Ethos",
+        body: "'WITH Mission' is an international missions organization with God's people who delight to worship Him, desire to have fellowship with Him and strive to make disciples of Jesus Christ, seeking to fulfill the Great Commission of Jesus Christ in the world."
+    },
+    {
+        id: 1,
+        name: "Purpose and Mission",
+        body: "The purpose and mission of 'WITH Mission' is to glorify God, compelled by God's great love and empowered by the Holy Spirit, by planting, strengthening, and partnering with churches around the world as we evangelize the unreached, disciple believers into churches, and equip churches to fulfill the Great Commission of Jesus Christ."
+    },
+    {
+        id: 2,
+        name: "Statement of Faith",
+        body: [
+            "God's Word : The Bible, consisting of the Old and New Testaments, is God's Word, fully inspired and without error in the original manuscripts, written under the inspiration of the Holy Spirit, and revealing for all peoples His character and purposes. It is the final authority in all matters relating to belief and behaviour.",
+            "The Triune God : There is only one living and true God who exists eternally in three persons: Father, Son and Holy Spirit. God is the almighty Creator, Saviour, and Judge who governs all things according to His sovereign will and is accomplishing His purposes in creation and in the Church to His glory.",
+            "God the Father : God the Father, an infinite, personal spirit, perfect in holiness, wisdom, power and love, is the source of all that is good. He initiates creation and redemption, which He accomplishes through His Son and the Holy Spirit.",
+            "Jesus the Son : Jesus Christ, God’s only begotten Son, conceived by the Holy Spirit, and both fully God and fully man, entered history as Saviour of the world. He was conceived of the Holy Spirit, born of a virgin, and lived an exemplary, sinless life in perfect submission to the Father and in loving relationships with others. He died on a cross, rose bodily and ascended to heaven where He is advocate for His people and is exalted as Lord of all.",
+            "The Holy Spirit : The Holy Spirit, who came forth from the Father and Son to convict the world of sin, righteousness, and judgment, and to regenerate, sanctify, and empower all who believe in Jesus Christ, makes the work of Christ effective to sinners, giving spiritual life and placing them into the Church. The Holy Spirit indwells every believer in Christ, and that He is an abiding helper, teacher and guide.",
+            "Salvation : Christ's sacrificial death, in which He bore the punishment due to sinners, is the only and all-sufficient basis of God's provision of salvation for all people of every culture and age, expressing His love and satisfying His justice. By God's grace the repentant sinner, through trusting alone in the Lord Jesus Christ as Saviour, is put right with God, adopted by the Father into His family and receives eternal life.",
+            "The Church: The universal Church is a living spiritual body of which Christ is the head and all regenerated persons are members.  It finds local expression in communities of believers called by God to worship, fellowship, proclaim the Gospel, and make disciples among all peoples, reflect God's character, engage in works of compassion, contend for truth and justice and celebrate baptism and communion.",
+            "The Human Race: Humanity is the climax of God's earthly creation, bearing His image, designed for relationship with Him, and being the object of His redeeming love. All people have sinned. This results in guilt, death, and alienation from God as well as the defacing of every aspect of human nature. People are unable to save themselves from sin’s penalty and power and from Satan's dominion.",
+        ]
+    },
+]
