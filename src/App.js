36f1aec2ab3cbc5fd581c23e10e612e341f92b3b
Yoo Choi <dchoiboi00@gmail.com>
2022-03-12 15:39:54 +0900

    logos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+import logo_blue from './img/logo_blue.png';
+import banner from './img/banner.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className='images'>
+          <img src={logo_blue} className="logo" alt="logo" />
+          <img src={banner} className="banner_hands" alt="logo" />
+        </div>
+        
         <p>
           Under Construction
         </p>
